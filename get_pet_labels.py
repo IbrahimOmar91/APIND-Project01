9cf55f85b19d63202bdb01729a5577ee134714a0
Ibrahim Omar <46249457+IbrahimOmar91@users.noreply.github.com>
2020-02-22 23:37:53 +0200

    Add files via upload

--- get_pet_labels.py
+++ get_pet_labels.py
@@ -3,9 +3,10 @@ from os import listdir
 def get_pet_labels(image_dir):
     results_dic = {}
     for file_name in listdir(image_dir):
-        patrs = file_name.split('_')
-        label = ' '.join(patrs[:-1]).strip().lower()
-        print (label)
-        results_dic[file_name] = [label]
+        if not file_name.startswith('.'):
+            patrs = file_name.split('_')
+            label = ' '.join(patrs[:-1]).strip().lower()
+            print (label)
+            results_dic[file_name] = [label]
 
     return results_dic
